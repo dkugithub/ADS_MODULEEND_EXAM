933397e991064c1a462525ea0dd4c5e15d89dc0d
dkugithub <100933780+dkugithub@users.noreply.github.com>
2022-05-05 17:25:15 +0530

    Add files via upload

---
+++ 220340520032/Q1/sorting.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class sorting{
+	static void insertionSort(int n, int[]a1){
+	for(int i=1;i<n;++i){
+	int key=a1[i];
+	int j=i-1;
+	while(j>=0 && a1[j]>key){
+	a1[j+1]=a1[j];
+	j=j-1;
+	printArray(a1);
+	}
+	a1[j+1]=key;
+  }
+}
+	static void printArray(int arr[])
+{
+	int n=arr.length;
+	for(int i=0;i<n;++i)
+System.out.print(arr[i]+ " ");
+System.out.println();
+}
+public static void main(String []args)
+{
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int arr[]={1,2,4,5,3};
+	for(int i=0;i<n;i++){
+		arr[i]=sc.nextInt();
+	}
+	insertionSort(5,arr);
+	printArray(arr);
+}
+}
